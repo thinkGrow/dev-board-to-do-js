0cf82501ea9101b70641b1d13ca85d8f9b5192f7
MaishaTahsin <rubaitreshad@gmail.com>
2025-05-23 09:26:16 +0600

    Implement add activity log for ALL CARDS - took me a while...

--- script/script.js
+++ script/script.js
@@ -13,18 +13,18 @@ for (let btn of btnComplete) {
 
     const container = document.getElementById("activity-container");
 
+    // Get task title from the same card
+    const taskCard = btn.closest(".task-card");
+    const taskTitle = taskCard.querySelector(".task-title").innerText;
+  
+
     const div = document.createElement("div");
-    const taskTitle = document.getElementsByClassName("task-title");
-    // console.log(taskTitle[1].innerText);
+    div.innerHTML = `
+      <div class="bg-blue-200 rounded-lg p-2 mt-10">
+        <p class="text-sm">You have completed the task: "${taskTitle}" at ${new Date().toLocaleTimeString()}</p>
+      </div>
+    `;
 
-    for (let title of taskTitle) {
-      div.innerHTML = `
-                              <div class="bg-blue-200 rounded-lg p-2 mt-10">
-                                <p class="text-sm">You have completed the task: "${title.innerText}" at 12:48:15 PM</p>
-                              </div>
-            `;
-
-      container.appendChild(div);
-    }
+    container.appendChild(div);
   });
 }
