71c69f6ae21a317e4fd8fb0023bfeaa8ab207647
MaishaTahsin <rubaitreshad@gmail.com>
2025-05-23 07:52:51 +0600

    activity task log addition functional for card1

--- script/script.js
+++ script/script.js
@@ -2,11 +2,23 @@ const btnComplete = document.getElementById("btn-complete");
 
 btnComplete.addEventListener("click", function (event) {
   event.preventDefault();
-  // "task-left"
 
-  let taskLeft = getInnerTextById("task-left") - 1;
-  // console.log(taskLeft);
+  let taskLeft = parseInt(getInnerTextById("task-left")) - 1;
+
   setInnerTextByIDandValue("task-left", taskLeft);
 
   btnComplete.disabled = true;
+
+  const container = document.getElementById("activity-container");
+
+  const div = document.createElement("div");
+  tasktTitle = getInnerTextById("task-title");
+
+  div.innerHTML = `
+                  <div class="bg-blue-200 rounded-lg p-2 mt-10">
+                    <p class="text-sm">You have completed the task: "${tasktTitle}" at 12:48:15 PM</p>
+                  </div>
+`;
+
+  container.appendChild(div);
 });
--- utilities/utilities.js
+++ utilities/utilities.js
@@ -1,14 +1,11 @@
 function getInputValueByID(id) {
   const value = document.getElementById(id).value;
-//   const convertedValue = parseFloat(value);
   return value;
 }
 
 function getInnerTextById(id) {
   const value = document.getElementById(id).innerText;
-  const convertedValue = parseFloat(value);
-
-  return convertedValue;
+  return value;
 }
 
 function setInnerTextByIDandValue(id, value) {
